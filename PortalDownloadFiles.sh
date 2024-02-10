13bcac3cd7db9a666e4f4635fd820e542ef3a7f7
antonioneves <antoniocalheirosneves@hotmail.com>
2024-02-10 22:18:46 +0000

    Created linux init script file

---
+++ PortalDownloadFiles.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Script to use the desktop application
+# to download files used in Portal Mais Transparência
+
+# Put this directory and its files inside your user folder (home).
+
+# Activate virtualenv
+# Launch application
+
+cd ~/dev/Tarefas-To-Do-List
+source .venv/bin/activate
+python main.py
+Agradecer a Deus
